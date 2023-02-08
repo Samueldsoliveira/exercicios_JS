72f15d8a7ce9ca17df656912fb2dcfe0a81a9f32
Samueldsoliveira <83595709+Samueldsoliveira@users.noreply.github.com>
2023-02-08 13:17:33 -0300

    code_estrelas

---
+++ estrelas/script.js
@@ -0,0 +1,8 @@
+var i;
+var asterisco;
+for(var linha = 1; linha <= 3; linha++){
+    for(var coluna = 1; coluna <= 10; coluna++){
+        document.write("*");
+    }
+    document.write("<br>");
+}
